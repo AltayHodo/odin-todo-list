89f0a3aea239cb244c109db0e0f09c7a2bcfbf28
Altay Hodoglugil <altay.hodo@gmail.com>
2024-05-23 00:04:09 -0700

    Add remove task functionality to DOM

--- src/modules/DOMHandler.js
+++ src/modules/DOMHandler.js
@@ -3,16 +3,33 @@ import toDo from "./toDo";
 const DOMHandler = (function () {
   const tasks = toDo.getTasks();
 
-  const container = document.querySelector('#container');
 
   const renderTasks = () => {
+    const container = document.querySelector('#container');
+    container.innerHTML = '';
+
     tasks.forEach((task, index) => {
       const taskItem = document.createElement('div');
-      taskItem.textContent = task.title;
-      taskItem.dataset.index = index;
+      taskItem.innerHTML = 
+        `
+         <input type="checkbox">
+         ${task.title}
+         <button class="remove-button" data-index="${index}">x</button>
+        `
+
       taskItem.classList.add('task-item');
       container.appendChild(taskItem);
     });
+    const removeButtons = document.querySelectorAll('.remove-button');
+    removeButtons.forEach(button => {
+      button.addEventListener('click', removeTask)
+    })
+  }
+
+  const removeTask = (e) => {
+    const index = e.target.dataset.index;
+    toDo.removeTask(index);
+    renderTasks();
   }
 
   return { renderTasks };
