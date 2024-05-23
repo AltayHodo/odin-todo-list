03c6d40c04af4993c095a86fe39ea14a239981f8
Altay Hodoglugil <altay.hodo@gmail.com>
2024-05-23 16:04:37 -0700

    Add project switching for UI

--- src/modules/DOMHandler.js
+++ src/modules/DOMHandler.js
@@ -11,7 +11,7 @@ const DOMHandler = (function () {
 
     tasks.forEach((task, index) => {
       const taskItem = document.createElement('div');
-      taskItem.innerHTML = 
+      taskItem.innerHTML =
         `
          <input type="checkbox" data-index="${index}" ${task.completed ? 'checked' : ''}>
          ${task.title}
@@ -21,9 +21,9 @@ const DOMHandler = (function () {
 
       taskItem.classList.add('task-item');
       taskItem.classList.add(`${task.priority}`);
-      if(task.completed){
+      if (task.completed) {
         taskItem.classList.add('completed')
-      } else{
+      } else {
         taskItem.classList.remove('completed');
       }
       container.appendChild(taskItem);
@@ -58,13 +58,32 @@ const DOMHandler = (function () {
     const sidebar = document.querySelector('#sidebar');
     const projContainer = document.querySelector('#projects-container');
     projContainer.innerHTML = '';
-    projects.forEach(project => {
+    projects.forEach((project, index) => {
       const projectItem = document.createElement('div');
       projectItem.textContent = project.name;
+      projectItem.dataset.index = index;
       projectItem.classList.add('project-item');
       projContainer.appendChild(projectItem);
     });
+    const projectItems = document.querySelectorAll('.project-item');
+    projectItems.forEach(item => {
+      item.addEventListener('click', switchProject)
+    })
   }
+
+  const switchProject = (e) => {
+    const index = e.target.dataset.index;
+    currentProject = projects[index];
+    renderTasks();
+  }
+
+  const switchToHome = () => {
+    currentProject = mainToDo;
+    renderTasks();
+  }
+  const home = document.querySelector('#home');
+  home.addEventListener('click', switchToHome);
+
 
   return { renderTasks };
 })();
