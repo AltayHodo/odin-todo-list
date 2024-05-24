a146147e3b624e45c3a3c5693fdf049660245f2a
Altay Hodoglugil <altay.hodo@gmail.com>
2024-05-23 17:08:13 -0700

    Add form validation for task form

--- src/modules/formHandler.js
+++ src/modules/formHandler.js
@@ -10,9 +10,26 @@ const formHandler = (function () {
     e.preventDefault();
 
     const currentProject = DOMHandler.getCurrentProject();
-    const title = taskForm.querySelector('input[name="title"]').value;
-    const priority = taskForm.querySelector('input[name="priority"]:checked').value;
+    const title = taskForm.querySelector('input[name="title"]').value.trim();
+    const priorityElement = taskForm.querySelector('input[name="priority"]:checked');
     const date = taskForm.querySelector('input[name="date"]').value;
+
+    const errorElement = document.querySelector('#form-error');
+    if (errorElement) {
+      errorElement.remove();
+    }
+
+    if (!title) {
+      alert('Please enter a title.');
+      return;
+    }
+
+    if (!priorityElement) {
+      alert('Please select a priority.');
+      return;
+    }
+
+    const priority = priorityElement.value;
     const task = new Task(title, priority, date);
     currentProject.addTask(task);
     DOMHandler.renderTasks();
@@ -22,7 +39,7 @@ const formHandler = (function () {
   const handleProjectFormSubmit = (e) => {
     e.preventDefault();
     const projectName = projectForm.querySelector('input[name="project-name"]').value;
-    if(projectName == ''){
+    if (projectName == '') {
       alert('Project name can\'t be empty');
       return;
     }
